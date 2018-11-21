c6611febc6f89e296f706ed7526a6598472474d5
Mihai Nicola <mihai.nicolla@gmail.com>
2018-11-22 00:41:44 +0200

    implement evaluation with monads
    lab #6

--- Interpreter/src/Evaluation/Applicative.hs
+++ Interpreter/src/Evaluation/Applicative.hs
@@ -3,15 +3,17 @@ module Evaluation.Applicative where
 import Syntax.Expression
 import Evaluation.Substitution
 import qualified Data.Map as M
+import Control.Monad.State
+
 
 {-|
-    Small-step applicative-order evaluation of a given expression,
-    within a given context.
+  Small-step applicative-order evaluation of a given expression,
+  within a given context.
 -}
 eval :: Expression             -- ^ Expression to be evaluated
-     -> Context                -- ^ Context where the evaluation takes place
-     -> (Expression, Context)  -- ^ Evaluation result, together with a possibly
-                               --   enriched context, in case of definition
+  -> Context                -- ^ Context where the evaluation takes place
+  -> (Expression, Context)  -- ^ Evaluation result, together with a possibly
+                 --   enriched context, in case of definition
 
 eval def@(Definition name e) context = (e, M.insert name e context)
 
@@ -20,10 +22,39 @@ eval var@(Var x) context = (M.findWithDefault var x context, context)
 eval lambda@(Lambda x e) context = (lambda, context)
 
 eval (Application var@(Var x) a) context = 
-    let evalVar = fst $ (eval var context)
-    in ((Application evalVar a), context)
+  let evalVar = fst $ (eval var context)
+  in ((Application evalVar a), context)
 eval (Application lambdaE@(Lambda x e) a) context = case a of
-    lambdaA@(Lambda x' e')  -> (subst x lambdaA e, context)
-    var@(Var x)             -> (subst x var e, context)
-    app@(Application e' a') -> ((Application lambdaE (fst $ eval app context)), context)
+  lambdaA@(Lambda x' e')  -> (subst x lambdaA e, context)
+  var@(Var x)             -> (subst x var e, context)
+  app@(Application e' a') -> ((Application lambdaE (fst $ eval app context)), context)
 eval e context = (e, context)
+
+
+type Eval = State Context
+evalM :: Expression -> Eval Expression
+
+evalM def@(Definition name e) = state $ \s -> (e, M.insert name e s)
+
+evalM var@(Var x) = do
+  context <- get
+  case M.lookup x context of
+    Nothing -> do
+      modify $ M.insert x var
+      return var
+    Just result -> return result
+
+evalM lambda@(Lambda x e) = return lambda
+
+evalM (Application var@(Var x) a) = do
+  evalVar <- evalM var
+  return (Application evalVar a)
+
+evalM (Application lambdaE@(Lambda x e) a) = case a of
+  lambdaA@(Lambda x' e')  -> return (subst x lambdaA e)
+  var@(Var x)             -> return (subst x var e)
+  app@(Application e' a') -> do
+    evalApp <- evalM app
+    return (Application lambdaE evalApp)
+
+evalM e = return e
--- Interpreter/src/Evaluation/Normal.hs
+++ Interpreter/src/Evaluation/Normal.hs
@@ -3,15 +3,16 @@ module Evaluation.Normal where
 import Syntax.Expression
 import Evaluation.Substitution
 import qualified Data.Map as M
+import Control.Monad.State
 
 {-|
-    Small-step normal-order evaluation of a given expression,
-    within a given context.
+  Small-step normal-order evaluation of a given expression,
+  within a given context.
 -}
 eval :: Expression             -- ^ Expression to be evaluated
-     -> Context                -- ^ Context where the evaluation takes place
-     -> (Expression, Context)  -- ^ Evaluation result, together with a possibly
-                               --   enriched context, in case of definition
+   -> Context                -- ^ Context where the evaluation takes place
+   -> (Expression, Context)  -- ^ Evaluation result, together with a possibly
+                 --   enriched context, in case of definition
 eval def@(Definition name e) context = (e, M.insert name e context)
 
 eval var@(Var x) context = (M.findWithDefault var x context, context)
@@ -19,8 +20,32 @@ eval var@(Var x) context = (M.findWithDefault var x context, context)
 eval lambda@(Lambda x e) context = (lambda, context)
 
 eval app@(Application e a) context = case e of                                        
-                                        Var x       -> 
-                                            let evalVar = fst $ (eval e context)
-                                            in ((Application evalVar a), context)
-                                        Lambda x e' -> (subst x a e', context)
-                                        _           -> (app, context)
+                    Var x       -> 
+                      let evalVar = fst $ (eval e context)
+                      in ((Application evalVar a), context)
+                    Lambda x e' -> (subst x a e', context)
+                    _           -> (app, context)
+
+
+type Eval = State Context
+evalM :: Expression -> Eval Expression
+
+evalM def@(Definition name e) = state $ \s -> (e, M.insert name e s)
+
+evalM var@(Var x) = do
+  context <- get
+  case M.lookup x context of
+    Nothing -> do
+      modify $ M.insert x var
+      return var
+    Just result -> return result
+
+evalM lambda@(Lambda x e) = return lambda
+
+evalM app@(Application e a) = case e of
+  Var x       -> do
+    evalVar <- evalM e
+    return (Application evalVar a)
+  Lambda x e' -> return (subst x a e')
+  _           -> return app
+    
